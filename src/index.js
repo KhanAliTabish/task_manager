0230b68a0ebe87faae1fa211da9812b8eb648800
KhanAliTabish <alitabishkhan.atk@gmail.com>
2020-07-13 21:19:00 +0530

    update and delete apis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/users')
 const Tasks = require('./models/tasks')
+const { findByIdAndDelete } = require('./models/users')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,6 +19,54 @@ app.post('/users', async (req, res) => {
    }
 })
 
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({})
+        if(!users) return res.status(404).send()
+        res.status(201).send(users)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user) return res.status(404).send()
+        res.status(201).send(user)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["name", "age", "email", "password"]
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidUpdate){
+        return res.status(400).send({"error": 'Invalid updates !!'})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators:true })
+        if(!user) return res.status(404).send()
+        res.status(201).send(user)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.status(201).send(user)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+
 app.post('/tasks', async (req, res) => {
     const task = new Tasks(req.body)
     try{
@@ -29,28 +78,10 @@ app.post('/tasks', async (req, res) => {
 
 })
 
-
-app.get('/users', async (req, res) => {
-    try{
-        const users = await User.find({})
-        res.status(201).send(users)
-    }catch(err){
-        res.status(400).send(err)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    try{
-        const user = await User.findById(req.params.id)
-        res.status(201).send(user)
-    }catch(err){
-        res.status(400).send(err)
-    }
-})
-
 app.get('/tasks', async (req, res) => {
     try{
         const tasks = await Tasks.find({})
+        if(!tasks) return res.status(404).send()
         res.status(201).send(tasks)
     }catch(err){
         res.status(400).send(err)
@@ -60,6 +91,33 @@ app.get('/tasks', async (req, res) => {
 app.get('/tasks/:id', async (req, res) => { 
     try{
         const task = await Tasks.findById(req.params.id)
+        if(!task) return res.status(404).send()
+        res.status(201).send(task)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidUpdate){
+        return res.status(400).send({"error": 'Invalid updates !!'})
+    }
+    try{
+        const task = await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators:true })
+        if(!task) return res.status(404).send()
+        res.status(201).send(task)
+    }catch(err){
+        res.status(400).send(err)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Tasks. findByIdAndDelete(req.params.id)
+        if(!task) return res.status(404).send()
         res.status(201).send(task)
     }catch(err){
         res.status(400).send(err)
