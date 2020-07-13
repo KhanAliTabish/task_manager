292fd80f9b3d4e35d85d8503b6d5bdaeb7913b5e
KhanAliTabish <alitabishkhan.atk@gmail.com>
2020-07-13 17:04:17 +0530

    find and findOne operations

--- mongodb.js
+++ mongodb.js
@@ -4,9 +4,9 @@ const {MongoClient, ObjectID} = mongodb
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task_manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
+// const id = new ObjectID()
+// console.log(id)
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionUrl, {useNewUrlParser: true}, (err, client) => {
     if(err){
@@ -15,28 +15,21 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true}, (err, client) => {
     console.log("Database connected successfully")
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name:'Tabish',
-    //     age:24
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log('Unable to insert Users')
-    //     }
-    //     console.log(result)
-    // })
+    db.collection('users').findOne({
+        name:'Tabish'
+    }, (err, user) => {
+        if(err){
+            return console.log('Unable to fetch Users')
+        }
+        console.log(user)
+    })
 
-    // db.collection('users').insertMany([{
-    //     name:'Interesting',
-    //     age:25
-    // },{
-    //     name: 'Something',
-    //     age:30
-    // }], (err, result) => {
-    //     if(err){
-    //         return console.log('Unable to insert Users')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection('users').find({ age: 23 }).toArray((err, users) => {
+        if(err){
+            return console.log('Unable to insert Users')
+        }
+        console.log(users)
+    })
 
     // db.collection('tasks').insertMany([{
     //     description:'learning nodeJs',
